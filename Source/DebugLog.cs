64e269d07026419e062120b738f9626926acaccd
Alex Tearse-Doyle <alextearsedoyle@gmail.com>
2019-03-19 07:41:36 -0700

    Empty Project

---
+++ Source/DebugLog.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Everybody_Gets_One
+{
+	static class Log
+	{
+		[System.Diagnostics.Conditional("DEBUG")]
+		public static void Message(string x)
+		{
+			Verse.Log.Message(x);
+		}
+	}
+}
---
+++ Source/Everybody_Gets_One.cs
@@ -0,0 +1,47 @@
+﻿using System.Reflection;
+using System.Linq;
+using Verse;
+using RimWorld;
+using UnityEngine;
+using Harmony;
+
+namespace Everybody_Gets_One
+{
+	public class Mod : Verse.Mod
+	{
+		public Mod(ModContentPack content) : base(content)
+		{
+			// initialize settings
+			// GetSettings<Settings>();
+#if DEBUG
+			HarmonyInstance.DEBUG = true;
+#endif
+
+			HarmonyInstance harmony = HarmonyInstance.Create("Uuugggg.rimworld.Everybody_Gets_One.main");
+
+			//Turn off DefOf warning since harmony patches trigger it.
+			MethodInfo DefOfHelperInfo = AccessTools.Method(typeof(DefOfHelper), "EnsureInitializedInCtor");
+			if (!harmony.GetPatchedMethods().Contains(DefOfHelperInfo))
+				harmony.Patch(DefOfHelperInfo, new HarmonyMethod(typeof(Mod), "EnsureInitializedInCtorPrefix"), null);
+			
+			harmony.PatchAll();
+		}
+
+		public static bool EnsureInitializedInCtorPrefix()
+		{
+			//No need to display this warning.
+			return false;
+		}
+
+//		public override void DoSettingsWindowContents(Rect inRect)
+//		{
+//			base.DoSettingsWindowContents(inRect);
+//			GetSettings<Settings>().DoWindowContents(inRect);
+//		}
+//
+//		public override string SettingsCategory()
+//		{
+//			return "Everybody Gets One";
+//		}
+	}
+}
