bd8ed37dd10f4961258f27092d0029410746caa7
Alex Tearse-Doyle <alextearsedoyle@gmail.com>
2022-02-08 13:07:06 -0800

    Store settings to static variable to use instead of looking them up everywhere

--- Source/Everybody_Gets_One.cs
+++ Source/Everybody_Gets_One.cs
@@ -9,10 +9,11 @@ namespace Everybody_Gets_One
 {
 	public class Mod : Verse.Mod
 	{
+		//public static Settings settings;
 		public Mod(ModContentPack content) : base(content)
 		{
 			// initialize settings
-			// GetSettings<Settings>();
+			// settings = GetSettings<Settings>();
 #if DEBUG
 			Harmony.DEBUG = true;
 #endif
@@ -25,7 +26,7 @@ namespace Everybody_Gets_One
 //		public override void DoSettingsWindowContents(Rect inRect)
 //		{
 //			base.DoSettingsWindowContents(inRect);
-//			GetSettings<Settings>().DoWindowContents(inRect);
+//			settings.DoWindowContents(inRect);
 //		}
 //
 //		public override string SettingsCategory()
--- Source/Settings.cs
+++ Source/Settings.cs
@@ -6,14 +6,9 @@ using RimWorld;
 
 namespace Everybody_Gets_One
 {
-	class Settings : ModSettings
+	public class Settings : ModSettings
 	{
 		public bool setting;
-
-		public static Settings Get()
-		{
-			return LoadedModManager.GetMod<Everybody_Gets_One.Mod>().GetSettings<Settings>();
-		}
 
 		public void DoWindowContents(Rect wrect)
 		{
