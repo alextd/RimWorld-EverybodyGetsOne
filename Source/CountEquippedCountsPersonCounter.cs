fbf8c3970895deab97f7649cdc8ff5c479117971
Alex Tearse-Doyle <alextearsedoyle@gmail.com>
2022-12-01 22:49:15 -0800

    Count equipped counts the counted people only. This does assume they're people

---
+++ Source/CountEquippedCountsPersonCounter.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+using RimWorld;
+using HarmonyLib;
+using TD_Find_Lib;
+
+namespace Everybody_Gets_One
+{
+
+	[HarmonyPatch(typeof(RecipeWorkerCounter), nameof(RecipeWorkerCounter.CountProducts))]
+	public static class CountEquippedCountsPersonCounter
+	{
+		public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+		{
+			// FreeColonistsSpawned is who it tracked equipped for.
+			MethodInfo FreeColonistsSpawnedInfo = AccessTools.PropertyGetter(typeof(MapPawns), nameof(MapPawns.FreeColonistsSpawned));
+
+			foreach (var inst in instructions)
+			{
+
+				if(inst.Calls(FreeColonistsSpawnedInfo))
+				{
+					yield return new CodeInstruction(OpCodes.Ldarg_1); //bill
+					yield return new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(CountEquippedCountsPersonCounter), nameof(FreeColonistsSpawnedOrCountedPawns)));
+				}
+				else
+					yield return inst;
+			}
+		}
+
+		public static List<Pawn> FreeColonistsSpawnedOrCountedPawns(MapPawns mapPawns, Bill_Production bill)
+		{
+			if (bill.repeatMode == RepeatModeDefOf.TD_PersonCount || bill.repeatMode == RepeatModeDefOf.TD_XPerPerson)
+			{
+				QuerySearch search = mapPawns.map.GetPersonCounter(bill);
+
+				search.RemakeList();
+
+				return search.result.allThings.Where(t => t is Pawn).Cast<Pawn>().ToList();
+			}
+			return mapPawns.FreeColonistsSpawned;
+		}
+	}
+}
